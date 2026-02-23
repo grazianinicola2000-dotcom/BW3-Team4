7268f8b0cf796153af701dee8f443d7eac707797
cicalecristian <cicalecristian@gmail.com>
2026-02-23 17:15:43 +0100

    NavBar quasi terminata

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import NavBarLink from "./components/NavBarLink"
 
 function App() {
-  return <></>;
+  return <NavBarLink />
 }
 
-export default App;
+export default App
